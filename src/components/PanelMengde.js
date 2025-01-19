5f7d45624c1bda704beaa60dff829bdd79b029d4
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-01-19 16:34:53 +0100

    Update PanelMengde.js

--- src/components/PanelMengde.js
+++ src/components/PanelMengde.js
@@ -3,18 +3,18 @@ import React from "react";
 export default function PanelMengde({ selectedPanelType, totalPanels }) {
   return (
     <div
-      className="bg-[#fff] rounded-3xl p-4 m-16 mx-auto max-w-lg gap-6 flex flex-col border-2"
+      className="bg-[#fff] rounded-3xl p-4 m-4 mx-auto max-w-lg gap-6 flex flex-col border-2"
       style={{ borderColor: "#FF9D00" }}
     >
       <div className="flex flex-row items-center justify-between mb-4">
-        <p className="text-lg font-semibold text-[#333]">
+        <p className="text-sm sm:text-lg font-semibold text-[#333]">
           Panelmengde <span className="font-bold">({selectedPanelType})</span>:
         </p>
-        <div className="px-4 py-2 border-2 border-[#ff9800] rounded-md bg-white text-[#333] font-semibold">
+        <div className="px-2 sm:px-4 py-1 sm:py-2 border-2 border-[#ff9800] rounded-md bg-white text-xs sm:text-base text-[#333] font-semibold">
           {totalPanels} paneler
         </div>
       </div>
-      <p className="text-sm text-[#666]">
+      <p className="text-xs sm:text-sm text-[#666]">
         Panelene leveres med 30 års produkt- og effektgaranti. Prisen inkluderer alt fra A-Å,
         uten skjulte kostnader – komplett solcelleanlegg.
       </p>
